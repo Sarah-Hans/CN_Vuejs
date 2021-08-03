d13a37e23298ddb372c6877dd76bc1864bb60c67
Sarah-Hans <leawine@gmail.com>
2021-08-03 08:41:36 +0200

    Premier livrable : modif champs input, ajout de class et affichage image

---
+++ app/script.js
@@ -0,0 +1,19 @@
+// rédaction du titre h1
+Vue.createApp({
+    data() {
+        return {
+            titre1: '',
+            titre2: '',
+            temptitre2: '',
+            isRed: false,
+            printImg: false,
+        }
+    },
+    methods: {
+        afficherTitre2() {
+            this.titre2 = this.temptitre2
+            },
+        }
+        
+}).mount('#app')
+
